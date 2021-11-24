36794bc445a293cf8a66a2c9febb896f4141c512
RozannevdBerk <r.m.v.d.berk@gmail.com>
2021-11-24 17:50:00 +0100

    created function, tested it somewhat
    I've created the function and it should work but haven't yet tested how best to integrate it with the other functions. My thought is maybe it would work as a method of the MiniMaxTree class but we'll see.

--- team5_A1/sudokuai.py
+++ team5_A1/sudokuai.py
@@ -29,3 +29,83 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
         while True:
             time.sleep(0.2)
             self.propose_move(random.choice(all_moves))
+            
+#=====================================================================================
+            
+def score_move(board_state: SudokuBoard, move: Move) -> float:
+    '''
+    Parameters
+    ----------
+    board_state : SudokuBoard
+        State of the current board BEFORE move is executed.
+    move : Move
+        Move to be executed, assumed to be legal.
+
+    Returns
+    -------
+    float
+        A measure of how likely the move is to lead to a new point.
+        Should be in the range of [-1,1], with 1 definitely leading to a new point 
+        and -1 very likely leading to the opponent scoring a new point.
+    '''
+    #apply move to the board, get new board
+    #new_board = game_state.board.copy()
+    new_board = SudokuBoard(board_state.m, board_state.n)
+    new_board.squares = board_state.squares.copy()
+    new_board.put(move.i,move.j,move.value)
+    
+    #count empty cells present in column, row, and block of the new move
+    #column, just loop over all values with the correct column index and count empty values
+    col_empty = 0
+    move_col = move.j
+    
+    for i in range(new_board.N):
+        if new_board.get(i,move_col) == new_board.empty:
+            col_empty += 1
+    
+    #row, same strategy as for column
+    row_empty = 0
+    move_row = move.i
+    
+    for j in range(new_board.N):
+        if new_board.get(move_row,j) == new_board.empty:
+            row_empty += 1
+    
+    #block, figure out where the edges of the block reside by calculating the remainder of 
+    #the fractions with block-width and -length
+    block_empty = 0
+    
+    remainder_width = move_row % new_board.m
+    left_border = move_row - remainder_width     #first index of current block
+    right_border = left_border + new_board.m     #first index of next block
+    
+    remainder_length = move_col % new_board.n
+    top_border = move_col - remainder_length     #first index of current block
+    bottom_border = top_border + new_board.n     #first index of next block
+    
+    for i in range(left_border, right_border):
+        for j in range(top_border, bottom_border):
+            if new_board.get(i,j) == new_board.empty:
+                block_empty += 1
+    
+    #compute column, row and block scores according to the heuristic
+    if col_empty%2 == 0: #if there's an even number of empty cells
+        col_score = 1 / (col_empty + 1)
+    else: #if there's an uneven number of empty cells
+        col_score = - (1 / col_empty)
+        
+    if row_empty%2 == 0:
+        row_score = 1 / (row_empty + 1)
+    else:
+        row_score = - (1 / row_empty)
+        
+    if block_empty%2 == 0:
+        block_score = 3 / (block_empty + 1)
+    else:
+        block_score = - (1 / block_empty)
+        
+    
+    final_score = (col_score + row_score + block_score) / 3
+                
+    return final_score
+    
---
+++ test-score_move.py
@@ -0,0 +1,50 @@
+from competitive_sudoku.sudoku import SudokuBoard, print_board, Move
+from team5_A1.sudokuai import score_move
+
+test_board = SudokuBoard()
+test_board.put(0,0,9)
+test_board.put(0,7,6)
+test_board.put(1,0,4)
+test_board.put(1,1,1)
+test_board.put(1,2,2)
+test_board.put(1,5,7)
+test_board.put(2,1,5)
+test_board.put(2,3,1)
+test_board.put(2,5,4)
+test_board.put(3,2,5)
+test_board.put(3,4,7)
+test_board.put(3,5,9)
+test_board.put(3,7,4)
+test_board.put(4,2,6)
+test_board.put(4,3,4)
+test_board.put(4,5,3)
+test_board.put(4,6,5)
+test_board.put(5,1,9)
+test_board.put(5,3,5)
+test_board.put(5,4,8)
+test_board.put(5,6,6)
+test_board.put(6,3,3)
+test_board.put(6,5,8)
+test_board.put(6,7,5)
+test_board.put(7,3,7)
+test_board.put(7,6,9)
+test_board.put(7,7,3)
+test_board.put(7,8,8)
+test_board.put(8,1,4)
+test_board.put(8,8,1)
+
+#moves made in order based on the scores
+test_board.put(3,3,6)
+test_board.put(1,4,6)
+test_board.put(0,6,1)
+test_board.put(0,8,4)
+
+print(print_board(test_board))
+
+possible_moves = [(2,0,6),(6,8,6),(1,8,5),(3,0,1),(1,8,5)]
+
+for m in possible_moves:
+    print(score_move(test_board,Move(*m)))
+    
+
+
