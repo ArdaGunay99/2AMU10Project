cf1eed034b89593a42dae082259b8fa93888d3a8
RozannevdBerk <r.m.v.d.berk@gmail.com>
2021-12-15 21:51:59 +0100

    added bar chart generating code

--- Analysis.py
+++ Analysis.py
@@ -181,10 +181,52 @@ for i in result_DF.index:
 
 #%%
 #create plot for boards x time
-x = [1,2,3,4,5]
+x = [1.5,4.5,7.5,10.5,13.5]
 x_labels = ['empty-3x3','random-3x3','hard-3x3','empty-4x4','random-4x4']
 
+empty3x3_01 = sum([result_arr[p,0,0] for p in range(4)]) / 20 * 100
+empty3x3_05 = sum([result_arr[p,0,1] for p in range(4)]) / 20 * 100
+empty3x3_10 = sum([result_arr[p,0,2] for p in range(4)]) / 20 * 100
+empty3x3_50 = sum([result_arr[p,0,3] for p in range(4)]) / 20 * 100
 
+random3x3_01 = sum([result_arr[p,1,0] for p in range(4)]) / 20 * 100
+random3x3_05 = sum([result_arr[p,1,1] for p in range(4)]) / 20 * 100
+random3x3_10 = sum([result_arr[p,1,2] for p in range(4)]) / 20 * 100
+random3x3_50 = sum([result_arr[p,1,3] for p in range(4)]) / 20 * 100
+
+hard3x3_01 = sum([result_arr[p,2,0] for p in range(4)]) / 20 * 100
+hard3x3_05 = sum([result_arr[p,2,1] for p in range(4)]) / 20 * 100
+hard3x3_10 = sum([result_arr[p,2,2] for p in range(4)]) / 20 * 100
+hard3x3_50 = sum([result_arr[p,2,3] for p in range(4)]) / 20 * 100
+
+empty4x4_01 = sum([result_arr[p,3,0] for p in range(4)]) / 12 * 100
+empty4x4_05 = sum([result_arr[p,3,1] for p in range(4)]) / 12 * 100
+empty4x4_10 = sum([result_arr[p,3,2] for p in range(4)]) / 12 * 100
+
+random4x4_01 = sum([result_arr[p,4,0] for p in range(4)]) / 12 * 100
+random4x4_05 = sum([result_arr[p,4,1] for p in range(4)]) / 12 * 100
+random4x4_10 = sum([result_arr[p,4,2] for p in range(4)]) / 12 * 100
+random4x4_50 = sum([result_arr[p,4,3] for p in range(4)]) / 12 * 100
+
+y1 = [empty3x3_01, random3x3_01, hard3x3_01, empty4x4_01, random4x4_01]
+y2 = [empty3x3_05, random3x3_05, hard3x3_05, empty4x4_05, random4x4_05]
+y3 = [empty3x3_10, random3x3_10, hard3x3_10, empty4x4_10, random4x4_10]
+y4 = [empty3x3_50, random3x3_50, hard3x3_50, 0, random4x4_50]
+
+x1 = [1,4,7,10,13]
+x2 = [2,5,8,11,14]
+
+plt.figure(4)
+plt.bar(x1,y1,width=-0.5,align='edge',label='0.1 second per turn',color='white',edgecolor='black',linewidth=0.65)
+plt.bar(x1,y2,width=0.5,align='edge',label='0.5 second per turn',color='darkgrey',edgecolor='black',linewidth=0.65)
+plt.bar(x2,y3,width=-0.5,align='edge',label='1.0 second per turn',color='dimgray',edgecolor='black',linewidth=0.65)
+plt.bar(x2,y4,width=0.5,align='edge',label='5.0 seconds per turn',color='black')
+plt.xlabel('boards')
+plt.ylabel('% of wins')
+plt.xticks(x,labels=x_labels)
+plt.yticks([0,10,20,30,40,50,60,70,80,90,100])
+plt.ylim(0,110)
+plt.legend()
 
 
 #%%
@@ -207,7 +249,7 @@ p2_random4x4 = sum([result_arr[p,4,t] for p in [1,3] for t in range(1,4)]) / 18 
 y1 = [p1_empty3x3, p1_random3x3, p1_hard3x3, p1_empty4x4, p1_random4x4]
 y2 = [p2_empty3x3, p2_random3x3, p2_hard3x3, p2_empty4x4, p2_random4x4]
 
-plt.figure(4)
+plt.figure(5)
 plt.bar(x,y1,width=0.3,align='edge',color='white',edgecolor='black',linewidth=0.65,label='team5_A2 is player 1')
 plt.bar(x,y2,width=-0.3,align='edge',color='grey',edgecolor='black',linewidth=0.65,label='team5_A2 is player 2')
 plt.xlabel('boards')
@@ -216,3 +258,23 @@ plt.xticks(x,labels=x_labels)
 plt.yticks([0,10,20,30,40,50,60,70,80,90,100])
 plt.ylim(0,110)
 plt.legend()
+
+#%%
+
+#create plot for p1 vs p2
+x = [1,2]
+x_labels = ["Player 1","Player 2"]
+
+p1 = sum([result_arr[p,b,t] for p in [0,2] for b in range(5) for t in range(1,4)]) / 120 * 100
+p2 = sum([result_arr[p,b,t] for p in [1,3] for b in range(5) for t in range(1,4)]) / 120 * 100
+
+y = [greedy, A1]
+
+plt.figure(1)
+plt.bar(x, y, width=0.5, color='grey',edgecolor='black',linewidth=0.65)
+plt.xticks(x, labels=x_labels)
+plt.xlim(0.5,2.5)
+plt.xlabel("Our agent's player nr")
+plt.yticks([0,10,20,30,40,50,60,70,80,90,100])
+plt.ylim(0,110)
+plt.ylabel("% of wins")
